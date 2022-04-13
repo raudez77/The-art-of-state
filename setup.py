84c973953dcfb012e91fa27764043d3c67f67aaf
Marvin Garcia <43289474+raudez77@users.noreply.github.com>
2022-04-13 15:34:59 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Canserbero',
+    version='1.0.0',
+    url='https://github.com/raudez77/The-are-of-state',
+    author='Marvin G.',
+    author_email='raudez77@hotmail.com',
+    description='Generating new lyrics',
+    packages=find_packages(),    
+)
